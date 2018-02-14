38f8be91980072986612ce10e243c7dc9c336e30
blabdouze <boris.labbe2@gmail.com>
2018-02-14 22:44:07 +0100

    feat(test): Add a test that generate all knowns maymay

--- tests/maymay_test.rb
+++ tests/maymay_test.rb
@@ -1,14 +1,38 @@
 require 'test/unit'
+require 'fileutils'
+
 require_relative '../model/maymay.rb'
 require_relative '../service/maymayService.rb'
+
+require_relative '../service/disrespectService.rb'
+require_relative '../handles/assetHandles.rb'
+
 
 class MaymayTest < Test::Unit::TestCase
 
   def setup
-    @maymays = MaymayService.load('tests/maymayTest.yml')
+    @maymays = MaymayService.load('config/maymay.yml')
+
+    @out_folder = Maymay::MAYMAY_OUT_FOLDER
+
+    # Create and clear folder if needed
+    Dir.mkdir(@out_folder) unless File.exists?(@out_folder)
+    FileUtils.rm_rf("#{@out_folder}/.", secure: true)
+  end
+
+  def test_generateDisrespect
+    puts "lol"
+    puts HANDLES::ASSET::randomize('lol','mdr')
   end
 
   def test_generate
-    @maymays['victime'].generate("Kikoo")
+    @maymays.each_value do |maymay|
+      assert(!File.exists?("#{@out_folder}/#{maymay.asset}"))
+      maymay.generate("test")
+      assert(File.exists?("#{@out_folder}/#{maymay.asset}"))
+    end 
   end
+
+
+
 end
